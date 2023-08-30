d4fd6b4d79acd8f3b0bf225ed5bddc313be0ae4b
Arishraaj <100360533+Arishraaj@users.noreply.github.com>
2023-08-30 21:54:14 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,52 @@
+quizzes = [
+    {
+        "question": "Which company produces the Mustang?",
+        "answer": "Ford",
+        "options": ["Chevrolet", "Toyota", "Ford", "Honda"]
+    },
+    {
+        "question": "What does SUV stand for?",
+        "answer": "Sport Utility Vehicle",
+        "options": ["Super Ultra Vroom", "Sports Undercarriage Vehicle", "Sport Utility Vehicle", "Speedy Urban Van"]
+    },
+    {
+        "question": "Which part of the car is responsible for combustion?",
+        "answer": "Engine",
+        "options": ["Steering Wheel", "Tires", "Engine", "Exhaust Pipe"]
+    },
+    {
+        "question": "What is the purpose of an alternator in a car?",
+        "answer": "To charge the battery and power the electrical system",
+        "options": ["To cool the engine", "To steer the car", "To charge the battery and power the electrical system", "To transmit power to the wheels"]
+    },
+    {
+        "question": "What type of fuel does a gasoline engine use?",
+        "answer": "Petrol/Gasoline",
+        "options": ["Diesel", "Electricity", "Petrol/Gasoline", "Natural Gas"]
+    },
+    {
+        "question": "Which part of the car is used to stop the vehicle?",
+        "answer": "Brakes",
+        "options": ["Gas pedal", "Steering wheel", "Brakes", "Accelerator"]
+    },
+    {
+        "question": "What is the purpose of a transmission in a car?",
+        "answer": "To change gears and transfer power to the wheels",
+        "options": ["To play music", "To store luggage", "To change gears and transfer power to the wheels", "To charge the battery"]
+    },
+    {
+        "question": "Which of these is a safety feature that helps protect the head in a car crash?",
+        "answer": "Airbags",
+        "options": ["Cup holders", "Sunroof", "Airbags", "DVD player"]
+    },
+    {
+        "question": "What do you call a car with two doors and a front seat?",
+        "answer": "Coupe",
+        "options": ["Sedan", "Convertible", "Coupe", "Hatchback"]
+    },
+    {
+        "question": "What is the function of a radiator in a car?",
+        "answer": "To cool the engine",
+        "options": ["To play radio channels", "To warm the interior", "To cool the engine", "To charge the battery"]
+    }
+]
